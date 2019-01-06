9241d1558531c15a74d6db26643d379e534bc8ca
Bertvb <pruttebol@hotmail.com>
2019-01-06 15:09:07 +0100

    4 componenten opnieuw toevoegen

---
+++ src/app/adressen/adressen.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdressenComponent } from './adressen.component';
+
+describe('AdressenComponent', () => {
+  let component: AdressenComponent;
+  let fixture: ComponentFixture<AdressenComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AdressenComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AdressenComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { LoginComponent } from './login/login.component';
+import { AdressenComponent } from './adressen/adressen.component';
+import { HelpComponent } from './help/help.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    LoginComponent,
+    AdressenComponent,
+    HelpComponent
   ],
   imports: [
     BrowserModule
