ea374ffa41376ad07a799700d35253e97ad3e143
Bertvb <pruttebol@hotmail.com>
2019-01-06 16:33:29 +0100

    bootstrap en routes added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
@@ -7,16 +11,27 @@ import { LoginComponent } from './login/login.component';
 import { AdressenComponent } from './adressen/adressen.component';
 import { HelpComponent } from './help/help.component';
 
+const MyRoutes: Routes = [
+  { path: '', component: IndexComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'adressen', component: AdressenComponent },
+  { path: 'help', component: HelpComponent }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent,
+    IndexComponent, 
     LoginComponent,
     AdressenComponent,
     HelpComponent
   ],
   imports: [
-    BrowserModule
+    NgbModule,
+    BrowserModule,
+    FormsModule,//les7
+    HttpModule,//les7
+    RouterModule.forRoot(MyRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
