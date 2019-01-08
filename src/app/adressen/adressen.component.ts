f034bc9a07095762610f546c77d68961d350c32f
Bertvb <pruttebol@hotmail.com>
2019-01-08 17:33:52 +0100

    Data weergeven met ID

--- src/app/adressen/adressen.component.ts
+++ src/app/adressen/adressen.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestoreCollection } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
+
+export interface User {
+  id?: string;
+  uID: string;
+  adressen: Array<any>;
+
+}
 
 @Component({
   selector: 'app-adressen',
@@ -8,11 +17,20 @@ import { AngularFirestore } from '@angular/fire/firestore';
   styleUrls: ['./adressen.component.css']
 })
 export class AdressenComponent implements OnInit {
-  users: Observable<any[]>;
-  
+  usersRef: AngularFirestoreCollection<User>;
+  usersId: Observable<User[]>;
+
   constructor(private db:AngularFirestore) {
-    this.users = db.collection('users').valueChanges();
+    this.usersRef = db.collection<User>('users');
+    this.usersId = this.usersRef.snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as User;
+        const id = a.payload.doc.id;
+        return {id, ...data };
+      }))
+    )
   }
+
   ngOnInit() {
   }
 
